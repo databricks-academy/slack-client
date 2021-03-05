a5c457bcf05a3d4649feff2b676eb08a2726a14a
Jacob D. Parr <jacob.parr@databricks.com>
2021-03-04 21:58:01 -0600

    More testing of setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ import setuptools
 from setuptools import find_packages
 
 setuptools.setup(
-    name="slack-client",
-    version="1.0.2",
+    name="slack_client",
+    version="1.0.3",
     packages=["slack_client"],
     package_dir={'': 'python'}
 )
